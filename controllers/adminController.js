778db3240dfd261a00b3afef1d6c9e0be6e139b0
AbdulMohsin325 <mohsinabdul325@gmail.com>
2026-02-17 17:24:18 +0530

    Added Admin login in adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 import Admin from "../models/Admin.js";
 import jwt from "jsonwebtoken";
 
+import bcrypt from "bcryptjs";
+
 export const registerAdmin = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -14,4 +16,29 @@ export const registerAdmin = async (req, res) => {
         success: true,
         message: "Admin registered successfully"
     });
+};
+
+
+export const loginAdmin = async (req, res) => {
+    const { email, password } = req.body;
+
+    const admin = await Admin.findOne({ email }).select("+password");
+
+    if (!admin) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+        { id: admin._id, role: "admin" },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+
+    res.json({ token });
 };
