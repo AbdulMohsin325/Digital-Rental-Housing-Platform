4766779a728e6f202c1c7b51746ecb4f67559880
AbdulMohsin325 <mohsinabdul325@gmail.com>
2026-02-19 13:19:46 +0530

    crud of booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,54 +1,102 @@
-// // import Booking from "../models/Booking.js";
-// // import House from "../models/House.js";
+import Booking from "../models/Booking.js";
+import House from "../models/House.js";
 
-// // export const createBooking = async (req, res) => {
-// //     try {
-// //         const { houseId, checkIn, checkOut } = req.body;
+//Booking is done by user only
+export const createBooking = async (req, res) => {
+    try {
+        const { houseId, startDate, endDate } = req.body;
 
-// //         const house = await House.findById(houseId);
+        const house = await House.findById(houseId);
 
-// //         // Check if house exists
-// //         if (!house) {
-// //             return res.status(404).json({ message: "House not found" });
-// //         }
+        // Check if house exists
+        if (!house) {
+            return res.status(404).json({ message: "House not found" });
+        }
 
-// //         const totalPrice = house.pricePerNight; // simple for now
+        const days =(new Date(checkOut) - new Date(checkIn)) /(1000 * 60 * 60 * 24);
 
-// //         // Create booking
-// //         const booking = await Booking.create({
-// //             user: req.user._id,
-// //             house: houseId,
-// //             checkIn,
-// //             checkOut,
-// //             totalPrice
-// //         });
+        const totalPrice = days * house.price; // simple for now
 
-// //         res.status(201).json({
-// //             success: true,
-// //             data: booking
-// //         });
+        // Create booking
+        const booking = await Booking.create({
+            user: req.user._id,
+            house: houseId,
+            startDate,
+            endDate,
+            totalPrice
+        });
 
-// //     } catch (error) {
-// //         res.status(400).json({ message: error.message });
-// //     }
-// // };
+        res.status(201).json({
+            success: true,
+            data: booking
+        });
 
-// // //Get My Bookings
-// // export const getMyBookings = async (req, res) => {
-// //     try {
-// //         const bookings = await Booking.find({ user: req.user._id })
-// //             .populate("house");
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
 
-// //         res.json({
-// //             success: true,
-// //             count: bookings.length,
-// //             data: bookings
-// //         });
+// Get My Bookings
+export const getMyBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find({ user: req.user._id })
+            .populate("house");
 
-// //     } catch (error) {
-// //         res.status(500).json({ message: error.message });
-// //     }
-// // };
+        res.json({
+            success: true,
+//             count: bookings.length,
+            data: bookings
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Admin get All booking
+
+export const getAllBookings = async (req, res) => {
+  try {
+    const bookings = await Booking.find()
+      .populate("user", "name email")
+      .populate("house", "title price");
+
+    res.json({
+      success: true,
+      data: bookings
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Cancel the Booking
+export const cancelBooking = async (req, res) => {
+  try {
+    const booking = await Booking.findById(req.params.id);
+
+    if (!booking) {
+      return res.status(404).json({ message: "Booking not found" });
+    }
+
+    if (
+      booking.user.toString() !== req.user._id.toString() &&
+      !req.user.isAdmin
+    ) {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+
+    booking.status = "cancelled";
+    await booking.save();
+
+    res.json({
+      success: true,
+      message: "Booking cancelled"
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 // import Booking from '../models/Booking.js';
 // import House from '../models/House.js';
