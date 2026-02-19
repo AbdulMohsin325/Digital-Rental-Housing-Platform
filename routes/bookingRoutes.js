aeb58e3a3553a5691ea4996cf94732e4eaf2b817
AbdulMohsin325 <mohsinabdul325@gmail.com>
2026-02-19 13:21:51 +0530

    protected routes for booking

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,24 +1,18 @@
-// import express from 'express';
-// import {
-//   createBooking,
-//   getMyBookings,
-//   getAllBookings,
-//   getBookingById,
-//   updateBooking,
-//   deleteBooking
-// } from '../controllers/bookingController.js';
+import express from "express";
+import {
+  createBooking,
+  getMyBookings,
+  getAllBookings,
+  cancelBooking
+} from "../controllers/bookingController.js";
 
-// import { protect, authorizeAdmin } from '../middleware/auth.js';
+import { protect, authorizeAdmin } from "../middleware/auth.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post('/', protect, createBooking);
-// router.get('/my', protect, getMyBookings);
+router.post("/", protect, createBooking);
+router.get("/my", protect, getMyBookings);
+router.get("/", protect, authorizeAdmin, getAllBookings);
+router.put("/:id/cancel", protect, cancelBooking);
 
-// router.get('/', protect, authorizeAdmin, getAllBookings);
-// router.get('/:id', protect, getBookingById);
-
-// router.put('/:id', protect, updateBooking);
-// router.delete('/:id', protect, deleteBooking);
-
-// export default router;
+export default router;
